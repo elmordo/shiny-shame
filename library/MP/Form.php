f389975810c042683138313c05a64ae9796b1e4f
Bc. Petr Jindra <el.mordo@gmail.com>
2014-01-17 00:28:36 +0100

    ignorace tech formularu, kterym nebyly odeslany data (validace vraci vzdy False)

--- library/MP/Form.php
+++ library/MP/Form.php
@@ -1,4 +1,20 @@
 <?php
 class MP_Form extends Zend_Form {
 	
+    /**
+     * nejprve zkonstroluje, jestou jsou nejaka data k dipozici
+     * 
+     * @param $data kontrolovana data
+     */
+    public function isValid($data) {
+        // vyhodnoceni, jestli je nastaveno elementsBelongsTo
+        if ($this->_elementsBelongTo) {
+            // vyhodnoceni jestli je tato hodnota v datech pritomna
+            if (!isset($data[$this->_elementsBelongTo])) {
+                return false;
+            }
+        }
+        
+        return parent::isValid($data);
+    }
 }
